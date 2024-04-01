2e39b764aeb6fae5010415004207f1515a21358b
renatoguara2020 <renatoguara2020@gmail.com>
2024-04-01 08:29:37 -0300

    ok

--- testes.py
+++ testes.py
@@ -1,9 +1,11 @@
 class Person:
-  def __init__(self, name, age):
+  def __init__(self, name, age, email):
     self.name = name
     self.age = age
+    self.email = email
 
-p1 = Person("John", 36)
+p1 = Person("John", 36, 'renatotaguatinga36@gmail.com')
 
 print(p1.name)
 print(p1.age)
+print(p1.email)
